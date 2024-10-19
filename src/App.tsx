02cde0c04897dd1fdc251fdf0ac40ebb10e8eb10
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2024-10-19 15:34:09 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
